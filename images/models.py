213161af4131e4c0f9e16516acbcee959adaa892
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-10 00:04:02 +0300

    creating location table and migrations

--- images/models.py
+++ images/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.location
+
+    class Meta:
+        ordering = ['location']
