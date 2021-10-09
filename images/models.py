c045b7b4e749be93b04198c12af96a5b678de33d
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-10 00:45:34 +0300

    create location methods and testing

--- images/models.py
+++ images/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 import datetime as dt
+from django.db.models import Q
 
 # Create your models here.
 class Location(models.Model):
     location = models.CharField(max_length=100)
+
+    # save location to database
+    def save_location(self):
+        self.save()
+
+    # update location
+    def update_location(self, location):
+        self.location = location
+        self.save()
+
+     # delete location from database
+    def delete_location(self):
+        self.delete()
 
     def __str__(self):
         return self.location
--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Location,Category,Image
+import datetime as dt
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.Moringa = Location(location='Moringa')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Moringa,Location))
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_save_method(self):
+        self.Moringa.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+    def test_delete_method(self):
+        self.Moringa.delete_location('Moringa')
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)==0)
