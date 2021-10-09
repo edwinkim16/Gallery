43402ac00c6c108e68edeb0453a3151102f278da
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-10 01:30:34 +0300

    test for image methods

--- images/tests.py
+++ images/tests.py
@@ -5,7 +5,7 @@ import datetime as dt
 # Create your tests here.
 class LocationTestClass(TestCase):
     def setUp(self):
-        self.Moringa = Location(location='Moringa')
+        self.Moringa = Location(location='London')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.Moringa,Location))
@@ -19,7 +19,7 @@ class LocationTestClass(TestCase):
         self.assertTrue(len(locations)>0)
 
     def test_delete_method(self):
-        self.Moringa.delete_location('Moringa')
+        self.Moringa.delete_location('London')
         locations = Location.objects.all()
         self.assertTrue(len(locations)==0)
 
@@ -42,3 +42,23 @@ class CategoryTestClass(TestCase):
         self.Food.delete_category('Football')
         category = Category.objects.all()
         self.assertTrue(len(category)==0)
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.test_category = Category(category=list('Football'))
+        self.test_category.save_category()
+
+        self.location = Location(location="London")
+        self.location.save_location()
+
+        self.image = Image(id=1,title="Football",categories=self.test_category,location=self.location,)
+        self.image.save_image()
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_save_image(self):
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
