9c4e7a742aee89f403480a4684721feabbbe3ec3
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-10 09:22:29 +0300

    search view

--- images/views.py
+++ images/views.py
@@ -11,3 +11,15 @@ def images(request):
    
 
     return render(request,'images.html',{"images":images})
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request,'search.html',{"message":message,"images":searched_images})
+
+    else:
+        message="You haven't searched for any term"
+        return render(request,'search.html',{"message":message})    
